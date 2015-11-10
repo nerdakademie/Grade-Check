64a36c1134e1eef39c6780b3c04376b34ba8efa1
Jan <jackflash5500@gmail.com>
2015-11-10 16:10:03 +0100

    Added auto-update

---
+++ Grade-Check.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        grade-check
+// @namespace   nak
+// @description checks for new grades
+// @include     https://cis.nordakademie.de/pruefungsamt/pruefungsergebnisse/?no_cache=1
+// @version     0.3
+// @grant       none
+// @downloadURL https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.user.js
+// @updateURL   https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.meta.js
+// ==/UserScript==
--- Grade-Check.user.js
+++ Grade-Check.user.js
@@ -3,8 +3,10 @@
 // @namespace   nak
 // @description checks for new grades
 // @include     https://cis.nordakademie.de/pruefungsamt/pruefungsergebnisse/?no_cache=1
-// @version     0.2.3
+// @version     0.3
 // @grant       none
+// @downloadURL https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.user.js
+// @updateURL   https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.meta.js
 // ==/UserScript==
 
 function reloadPage(){
