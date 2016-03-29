f4e8f4db8c61712073c44e66cced16a6f81a31cb
Michel Wohlert <michel.wohlert@gmail.com>
2016-03-29 13:34:45 +0200

    Actually commit

--- Grade-Check.user.js
+++ Grade-Check.user.js
@@ -3,7 +3,7 @@
 // @namespace   nak
 // @description checks for new grades
 // @include     https://cis.nordakademie.de/pruefungsamt/pruefungsergebnisse/?no_cache=1
-// @version     0.4.4
+// @version     0.4.5
 // @grant       none
 // @downloadURL https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.user.js
 // @updateURL   https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.meta.js
@@ -29,9 +29,9 @@ function average(arr){
         credsum = credsum + parseFloat(credpoint);
       }
     }
-    if(gradesum !=0 && credsum !=0){
+    if(gradesum !==0 && credsum !==0){
       return (Math.round(gradesum/credsum*1000)/1000);
-    }  
+    }
   }
   return "";
 }
@@ -41,16 +41,16 @@ function comparing() {
     var oldTableArray = JSON.parse(localStorage.getItem("oldTableArray"));
     var compareTable = document.getElementsByClassName("table");
     var compareTableContent = compareTable[0].textContent;
-    
+
     if (compareTableContent != oldTableContent) {
       var subjects = "",array = new Array();
-      if(oldTableArray == null){
+      if(oldTableArray === null){
         oldTableArray = new Array();
       }
       for (var i = 1, row; row = compareTable[0].rows[i]; i++) {
         if(i < compareTable[0].rows.length-1){ //Ignore last row
           if(oldTableArray.length > i ){
-              if(!(oldTableArray[i][1] === row.cells[4].textContent.trim()) && row.cells[4].textContent.trim().length > 0){
+              if(oldTableArray[i][1] !== row.cells[4].textContent.trim() && row.cells[4].textContent.trim().length > 0){
               //Grade has changed
               array.push([row.cells[1].textContent.trim() , row.cells[4].textContent.trim() , row.cells[5].textContent.trim()]);
             }
@@ -66,7 +66,7 @@ function comparing() {
       }
       if(array.length > 1){
         alert('Es gibt neue Noten in den Fächern: ' +subjects);
-      }else if (array.length = 1){
+      }else if (array.length === 1){
         alert('Es gibt eine neue Note in dem Fach ' + subjects);
       }
       var oldTable = document.getElementsByClassName("table");
@@ -76,7 +76,7 @@ function comparing() {
     }
     compareTable[0].rows[compareTable[0].rows.length-1].cells[4].textContent = average(oldTableArray);
     setTimeout(reloadPage,60000);
-};
+}
 
 
 comparing();
@@ -95,6 +95,21 @@ var colorTable= {
   '5,0' : '#FF6666'
 };
 
+function getClosestValue(aNum){
+    var gradeNum = parseFloat(aNum.replace(',','.'));
+    var diff = 100,
+    closestkey= '';
+    Object.keys(colorTable).forEach(function(key, index) {
+        var keyNum = parseFloat(key.replace(',','.'));
+        if(Math.abs(keyNum - gradeNum) < diff){
+            closestkey = key;
+            diff = Math.abs(keyNum - gradeNum);
+        }
+
+    }, colorTable);
+    return colorTable[closestkey];
+}
+
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
@@ -103,12 +118,13 @@ for(var i = 1; i <= 31; i++){
   var elem = getElementByXpath('//*[@id="curricular"]/table/tbody/tr[' + i + ']/td[5]');
   if (elem !== null){
     if(colorTable[elem.innerText.trim().substring(0,3)] !== undefined){
-      elem.setAttribute('bgcolor', colorTable[elem.innerText.trim().substring(0,3)]);
-      elem.style.backgroundColor = colorTable[elem.innerText.trim().substring(0,3)];
+      elem.setAttribute('bgcolor', getClosestValue(elem.innerText.trim().substring(0,3)));
+      elem.style.backgroundColor = getClosestValue(elem.innerText.trim().substring(0,3));
     }
   }
 }
 
+//Average coloration
 var elem = getElementByXpath('//*[@id="curricular"]/table/tbody/tr[31]/th[5]');
-elem.setAttribute('bgcolor', colorTable[elem.innerText.replace('.',',')]);
-elem.style.backgroundColor = colorTable[elem.innerText.replace('.',',')];
+elem.setAttribute('bgcolor', getClosestValue(elem.innerText.replace('.',',')));
+elem.style.backgroundColor = getClosestValue(elem.innerText.replace('.',','));
