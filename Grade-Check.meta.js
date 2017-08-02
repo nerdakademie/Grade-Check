0cdae7a5528661e2006cb920605338258b54682a
Michel Wohlert <michel.wohlert@gmail.com>
2017-08-02 15:13:43 +0200

    Update Grade-Check.meta.js

--- Grade-Check.meta.js
+++ Grade-Check.meta.js
@@ -3,7 +3,8 @@
 // @namespace   nak
 // @description checks for new grades
 // @include     https://cis.nordakademie.de/studium/pruefungen/pruefungsergebnisse/
-// @version     0.4.14
+// @include     https://cis.nordakademie.de/mein-profil/mein-postfach/leistungsuebersicht/
+// @version     0.4.15
 // @grant       none
 // @downloadURL https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.user.js
 // @updateURL   https://github.com/nerdakademie/Grade-Check/raw/master/Grade-Check.meta.js
